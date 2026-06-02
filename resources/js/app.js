0b2929b693747a72e5cdb6c5525267cc28845911
andhika <andhikasyhptr81@gmail.com>
2026-06-03 01:23:57 +0700

    fix frontend css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-//
+import '../css/app.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            navLinks.classList.toggle('active');
+        });
+    }
+});
