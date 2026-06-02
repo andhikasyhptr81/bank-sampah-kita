7e6897336b07928ba704c542523dc04cad1b2eb4
andhika <andhikasyhptr81@gmail.com>
2026-06-02 21:14:14 +0700

    fix cache driver

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
+
 <body>
     @include('components.navbar')
 
@@ -22,4 +24,5 @@
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
 </body>
+
 </html>
